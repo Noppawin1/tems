41b9e05382f0598ed03623464ff134b44a78dd71
noppawin kaewkongsri <tems.12363@gmail.com>
2022-10-19 10:08:03 +0700

    first commit

--- test.php
+++ test.php
@@ -1,1 +1,125 @@
-5555
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="Style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </head>
+        <title>Welcom to Firebase Hosting</title>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i class="fas fa-bars"></i>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="main-form.html">หน้าหลัก</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="detail.html">สินค้า</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="promotion.html">โปรโมชั่น</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pay.html">การชำระเงิน</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.html">ติดต่อเรา</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="feedback.html">Comments</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="price.html">ใบเสร็จ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="Customer\selectC.php">ลูกค้า</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
+        
+    <div class="h-75 d-flex justify-content-center align-items-center">
+            <div class="col-md-4 p-5 shadow-sm border rounded-5 border-primary">
+                <h2 class="text-center mb-4 text-primary">Login Form</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="leInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control bg-info bg-opacity-10 border border-primary" id="leInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="leInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control bg-info bg-opacity-10 border border-primary" id="leInputPassword1">
+                    </div>
+                    <p class="small"><a class="text-primary" href="forget-password.html">Forgot password?</a></p>
+                    <div class="d-grid">
+                        <button class="btn btn-primary" type="submit" >Login</button>
+                        
+                    </div>
+                </form>
+                <div class="mt-3">
+                    <p class="mb-0  text-center">Don't have an account? <a href="signup.html"
+                            class="text-primary fw-bold">Sign
+                            Up</a></p>
+                </div>
+            </div>
+        </div>
+        
+        <footer class="text-center text-lg-start bg-white text-muted">
+            <!-- Section: Social media -->
+            <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+              
+            </section>
+            <!-- Section: Social media -->
+          
+            <!-- Section: Links  -->
+            <section class="">
+              <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                  <!-- Grid column -->
+                  <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-9">
+                      <i class="fas fa-gem me-8 text-secondary"></i>หน้าเว็บนี้ออกแบบโดย อ.เทม
+                    </h6>
+                    <p>
+                      ลิขสิทธ์ของคอมหลักสูตรวิทยาการคอมพิวเตอร์
+                    </p>
+                  </div>
+          
+                  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                    <p><i class="fas fa-home me-3 text-secondary"></i> Facebook: Noppawin tems</p>
+                    <p>
+                      <i class="fas fa-envelope me-3 text-secondary"></i>
+                      Email: tems.12363@mail.com
+                    </p>
+                  
+                  </div>
+                  <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+              </div>
+            </section>
+           
+          </footer>
+    </body>
+</html>
